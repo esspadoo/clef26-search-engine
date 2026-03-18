0717ce25dfbca016dc951d97c191f452c89102dc
giancarlopadoan <giancarlo.padoan@studenti.unipd.it>
2026-03-18 13:08:37 +0100

    Insert loadPapers, loadQueries in DataLoader

--- code/src/main/java/unipd/se/DataLoader.java
+++ code/src/main/java/unipd/se/DataLoader.java
@@ -1,5 +1,25 @@
 package unipd.se;
 
+import unipd.se.model.Paper;
+import unipd.se.model.QueryDoc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class DataLoader {
 
+    public static List<Paper> loadPapers(String path) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Paper[] papers = mapper.readValue(new File(path), Paper[].class);
+        return Arrays.asList(papers);
+    }
+
+    public static List<QueryDoc> loadQueries(String path) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        QueryDoc[] queries = mapper.readValue(new File(path), QueryDoc[].class);
+        return Arrays.asList(queries);
+    }
 }
