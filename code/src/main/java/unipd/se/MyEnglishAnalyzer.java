c0c997d8ac65845687845bfb979a2e261672b338
Fabio Baldan <fabio.baldan.1@studenti.unipd.it>
2026-04-03 13:25:57 +0000

    MyEnglishAnalyzer_V2

--- code/src/main/java/unipd/se/MyEnglishAnalyzer.java
+++ code/src/main/java/unipd/se/MyEnglishAnalyzer.java
@@ -3,33 +3,41 @@ package unipd.se;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.en.*;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.StopFilter;
-import org.tartarus.snowball.ext.EnglishStemmer; // Snowball stemmer
-import org.apache.lucene.analysis.snowball.SnowballFilter;
-import org.apache.lucene.analysis.en.EnglishPossessiveFilter;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.analysis.pattern.PatternReplaceFilter;
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.miscellaneous.TrimFilter;
 
+import java.io.IOException;
 import java.util.regex.Pattern;
+
+//Added to read custom stoplist instead of the ENGLISH_STOP_WORDS_SET default one
+import org.apache.lucene.analysis.CharArraySet;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 
 /**
  * Custom analyzer for social-post-like queries and scientific documents.
- * It normalizes input text by removing hashtag prefixes, trimming tokens,
- * lowercasing text, removing English stopwords and possessives, normalizing
- * accented characters, and applying stemming.
- *
- * @author RETRIX
- * @version 1.0
- * @since 1.0
+ * <p>
+ * Features:
+ * - Removes '#' from hashtags (#anxiety → anxiety)
+ * - Lowercases text
+ * - Removes stopwords
+ * - Applies stemming (Snowball stemmer)
+ * - Normalizes accents
  */
-public class MyEnglishAnalyzer extends Analyzer {
+public class MyEnglishAnalyzer_V2 extends Analyzer {
 
     // Remove '#' only at the beginning of tokens
     private static final Pattern HASHTAG_PATTERN = Pattern.compile("^#");
+
+    // regex per rimuovere menzioni (@)
+    private static final Pattern MENTION_PATTERN = Pattern.compile("@\\w+");
 
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
@@ -37,11 +45,25 @@ public class MyEnglishAnalyzer extends Analyzer {
         Tokenizer tokenizer = new StandardTokenizer();
         TokenStream stream = tokenizer;
 
+        //included custom stopwords list
+        CharArraySet stopWords = new CharArraySet(16, true);
+
+        try {
+            List<String> lines = Files.readAllLines(Path.of("/<your_PATH>/seupd2526-retrixLOCALE/code/data/stoplist_en_ranksnl_large.txt"));
+            stopWords.addAll(lines);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         // Trim tokens
         stream = new TrimFilter(stream);
 
         // Remove '#' from hashtags
         stream = new PatternReplaceFilter(stream, HASHTAG_PATTERN, "", true);
+
+        //DA TESTARE
+        //Rimuovo menzioni
+        //stream = new PatternReplaceFilter(stream, MENTION_PATTERN, "", true);
 
         // Normalize accents
         stream = new ASCIIFoldingFilter(stream);
@@ -53,11 +75,10 @@ public class MyEnglishAnalyzer extends Analyzer {
         stream = new EnglishPossessiveFilter(stream);
 
         // Remove stopwords
-        stream = new StopFilter(stream, EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
+        stream = new StopFilter(stream, stopWords);
 
         // Apply stemming
-        stream = new SnowballFilter(stream, new EnglishStemmer());
-
+        stream = new KStemFilter(stream);
         return new TokenStreamComponents(tokenizer, stream);
     }
 }
